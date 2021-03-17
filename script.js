ed8f5b48e776ca7cc09157de7efbe61d962318e3
WALID ALRIFAI <78854936+alwalidalrifai@users.noreply.github.com>
2021-03-17 22:34:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function result()
-{
-amount = document.getElementById("amount").value;
-number = document.getElementById("number").value;
-Numberofyears = document.getElementById("Numberofyears").value;
+/*This function is made to create the result and alert for negative or zero Amount of money*/
+function result(){
+var amount = document.getElementById("amount").value;
+var number = document.getElementById("number").value;
+var Numberofyears = document.getElementById("Numberofyears").value;
+
+if (amount <= 0) {
+    alert("Enter a Positive value");
+    return false;
+  }
 document.getElementById("result").innerHTML = (amount*(number/100)*Numberofyears);
 }
+
+/*Simple function made for the range slider for interest rate*/
+function show_value(x)
+{
+ slider_value = document.getElementById("slider_value").innerHTML=x;
+}
+
